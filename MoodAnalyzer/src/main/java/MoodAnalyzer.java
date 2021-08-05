3e4bfc62cb49e02eefcfa3c08cfaaa36e47f6283
chaitra845 <chaitrachytra@gmail.com>
2021-08-05 18:30:19 +0530

    Refactored the Code

--- MoodAnalyzer/src/main/java/MoodAnalyzer.java
+++ MoodAnalyzer/src/main/java/MoodAnalyzer.java
@@ -1,6 +1,14 @@
 public class MoodAnalyzer {
 
-    public String analyzeMood(String msg) {
+    private String msg;
+    public MoodAnalyzer() {
+    }
+
+    public MoodAnalyzer(String msg){
+        this.msg = msg;
+    }
+
+    public String analyzeMood() {
         if(msg.contains("Sad")){
             return "Sad";
         }
